7acadeb3f8b7e4afc987d797d3162cdcbc817a7f
Minniefo <minindifonseka@gmail.com>
2025-04-27 21:28:11 +0530

    Created Question Document

---
+++ skillshare/src/main/java/com/edu/skillshare/document/Question.java
@@ -0,0 +1,24 @@
+package com.edu.skillshare.document;
+
+import jdk.jfr.Name;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collation = "questions")
+public class Question {
+    @Id
+    private String id;
+    private String questionText;
+    private List<String> options; // Size = 4
+    private int correctOptionIndex; // Index of the correct answer in the options list (0-3)
+}
