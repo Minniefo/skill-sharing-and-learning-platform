1a452700355e787ad54ebfd7fd9a150e18d47069
Minniefo <minindifonseka@gmail.com>
2025-05-12 22:19:31 +0530

    setup firebase

--- skillshare/src/main/java/com/edu/skillshare/repository/QuestionRepository.java
+++ skillshare/src/main/java/com/edu/skillshare/repository/QuestionRepository.java
@@ -2,7 +2,9 @@ package com.edu.skillshare.repository;
 
 import com.edu.skillshare.document.Question;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends MongoRepository<Question,String> {
 }
 
