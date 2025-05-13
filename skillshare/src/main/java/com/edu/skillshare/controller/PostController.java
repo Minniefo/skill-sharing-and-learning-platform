651b2caa49b3643c4b1276168684bb0b02b290d8
rajanaNulka <it22071934@my.sliit.lk>
2025-05-13 14:30:37 +0530

    created post controller

---
+++ skillshare/src/main/java/com/edu/skillshare/controller/PostController.java
@@ -0,0 +1,43 @@
+package com.edu.skillshare.controller;
+
+import com.edu.skillshare.dto.PostReq;
+import com.edu.skillshare.dto.PostResponse;
+import com.edu.skillshare.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/posts")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class PostController {
+    private  final PostService postService;
+
+    @PostMapping
+    public PostResponse createPost(@RequestBody PostReq dto) {
+        return postService.createPost(dto);
+    }
+
+    @GetMapping("/{id}")
+    public PostResponse getPostById(@PathVariable String id) {
+        return postService.getPostById(id);
+    }
+
+    @GetMapping
+    public List<PostResponse> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @PutMapping("/{id}")
+    public PostResponse updatePost(@PathVariable String id, @RequestBody PostReq dto) {
+        return postService.updatePost(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable String id) {
+        postService.deletePost(id);
+    }
+}
---
+++ skillshare/src/main/java/com/edu/skillshare/service/PostServiceImpl.java
@@ -0,0 +1,70 @@
+package com.edu.skillshare.service;
+
+import com.edu.skillshare.document.Post;
+import com.edu.skillshare.dto.PostReq;
+import com.edu.skillshare.dto.PostResponse;
+import com.edu.skillshare.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+    private final PostRepository postRepository;
+
+    @Override
+    public PostResponse createPost(PostReq dto) {
+        Post post = Post.builder()
+                .postName(dto.getPostName())
+                .postDescription(dto.getPostDescription())
+                .postImage(dto.getPostImage())
+                .build();
+        return mapToDto(postRepository.save(post));
+    }
+
+    @Override
+    public PostResponse getPostById(String id) {
+        return postRepository.findById(id)
+                .map(this::mapToDto)
+                .orElse(null);
+    }
+
+    @Override
+    public List<PostResponse> getAllPosts() {
+        return postRepository.findAll()
+                .stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public PostResponse updatePost(String id, PostReq dto) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            post.setPostName(dto.getPostName());
+            post.setPostDescription(dto.getPostDescription());
+            post.setPostImage(dto.getPostImage());
+            return mapToDto(postRepository.save(post));
+        }
+        return null;
+    }
+
+    @Override
+    public void deletePost(String id) {
+        postRepository.deleteById(id);
+    }
+
+    private PostResponse mapToDto(Post post) {
+        return PostResponse.builder()
+                .id(post.getId())
+                .postName(post.getPostName())
+                .postDescription(post.getPostDescription())
+                .postImage(post.getPostImage())
+                .build();
+    }
+}
